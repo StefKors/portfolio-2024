3f8454a3462548de98de79a942c9ec4a91b2b3a8
Stef Kors <stef.kors@gmail.com>
2024-10-20 15:23:18 +0200

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-
+  modules: ['@nuxt/content', '@nuxthq/studio'],
+  // force module initialization on dev env
+  // studio: {
+  //   enabled: true
+  // },
   routeRules: {
     '/': { prerender: true }
   },
